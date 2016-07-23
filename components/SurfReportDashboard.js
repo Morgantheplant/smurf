e0cd9e34f6326303e0c02b0c59c720cd9b152f0e
Morgan Plant <morgantheplant@gmail.com>
2016-07-23 12:10:47 -0700

    add dynamic data to surf report container

--- components/SurfReportDashboard.js
+++ components/SurfReportDashboard.js
@@ -9,31 +9,31 @@ class SurfReportDashboard extends React.Component {
     return (
     <div id="firstReport" classNames="topBoxes surfReport">
       <div className="waveHeight">
-        <h2 className="hoverHide">3-4</h2><span>ft</span>
+        <h2 className="hoverHide">{this.props.surfHeightRange}</h2><span>ft</span>
       </div>  
       <div className="conditionIcons">
         <ul>
-          <li>Fair to Good</li>
+          <li>{this.props.conditionsText}</li>
           <li className="hoverHide">3 knts SW</li>
           <li className="hoverHide">Rising 12:00pm .7m</li>
           <li><span>Water : 72</span> <span>Air : 80</span></li>
         </ul>
       </div>
       <div className="clear"></div>
-      <p className="hoverHide"></p>
+      <p className="hoverHide">{this.props.forecaseSummaryText}</p>
     </div>
       )
   }
 }
 
-// SurfReportDashboard.propTypes = {
-//   location: React.PropTypes.string,
-//   datePretty: React.PropTypes.string,
-//   surfHeightRange: React.PropTypes.string,
-//   surfHeightRangeText: React.PropTypes.string,
-//   conditionIcon: React.PropTypes.string,
-//   conditionsText: React.PropTypes.string,
-//   forecaseSummaryText: React.PropTypes.string
-// }
+SurfReportDashboard.propTypes = {
+  // location: React.PropTypes.string,
+  // datePretty: React.PropTypes.string,
+  surfHeightRange: React.PropTypes.string,
+  surfHeightRangeText: React.PropTypes.string,
+  // conditionIcon: React.PropTypes.string,
+  conditionsText: React.PropTypes.string,
+  forecaseSummaryText: React.PropTypes.string
+}
 
 export default SurfReportDashboard;
