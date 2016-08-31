a8c598503901e04f1f00f361dabd16c4355ca023
Morgan Plant <morgantheplant@gmail.com>
2016-08-30 22:53:34 -0700

    refactor animation

--- mainAnimation.js
+++ mainAnimation.js
@@ -1,188 +1,197 @@
-var Matter = require('matter-js');
-var surf = require('./data');
-var dataBuilder = require('./dataBuilder.js');
-var mainAnimationLoop = require('./mainAnimationLoop');
-var store = require('./reducers/rootStore').default;
-var hoverDay =  require('./actions/clockActions').hoverDay;
+import { World, Bodies, Body, Render, Composite, Composites, Constraint , Engine } from 'matter-js';
+import dataBuilder from './dataBuilder.js';
+import mainAnimationLoop from './mainAnimationLoop';
+import store from './reducers/rootStore';
+import { hoverDay } from './actions/clockActions';
 
-var World = Matter.World,
-Bodies = Matter.Bodies,
-Body = Matter.Body,
-Render = Matter.Render,
-Composite = Matter.Composite,
-Composites = Matter.Composites,
-Constraint = Matter.Constraint;
+// json 
+let surf = require('./data'); 
 
+let svgns = "http://www.w3.org/2000/svg",
+ary = dataBuilder(surf.ob)
 
+let width = 43,
+  height = 20,
+  padding = 3,
+  initColor = "#26333c",
+  initHighlight = "#014971";  
 
-var svgns = "http://www.w3.org/2000/svg";
-var ary = dataBuilder(surf.ob)
-
-//width and height of svg elements
-var width = 43;
-var height = 20;
-var padding = 3;
-var initColor = "#26333c";
-var initHighlight = "#014971";
-
-var Engine = Matter.Engine,
-    World = Matter.World,
-    Bodies = Matter.Bodies;
-
-var engine; 
-var svgBG = document.getElementById('world');
-var matterEngine;
-var svgBods = [];
-var svgBoxDims = svgBG.getBoundingClientRect();
-var yCoord = svgBoxDims - height;
-var highlighted = 0;
-var physicsBodies = [];
-var vertSprings = [];
-var horzSprings = [];
-var svgBars = [];
-
-function reset(){
-  mainAnimationLoop.removeAnimation(matterEngine);
-  mainAnimationLoop.removeAnimation(render);
-  physicsBodies = [];
-  vertSprings = [];
-  horzSprings = [];
+function SurfReportAnimation(options){
+  this.engine = null;
+  this.svgBods = [];
+  this.highlighted = 0;
+  this.physicsBodies = [];
+  this.vertSprings = [];
+  this.horzSprings = [];
+  this.svgBars = [];
+  this.mainBackground = options.$el;
+  this.$el = {
+    $el: options.$el
+  }
+  this.svgBoxDims = options.$el.getBoundingClientRect(); 
+  this.mainAnimationLoop = options.mainAnimationLoop;
+  this.name = options.name
+  this.mainBackground.style.opacity = 1;
+  this.mainBackground.style.visibility = "visible";
 }
 
-function createBodies(days){
+SurfReportAnimation.prototype.reset = function reset(){
+  console.log('reset')
+  this.mainAnimationLoop.removeAnimation(this.matterEngine.bind(this));
+  this.mainAnimationLoop.removeAnimation(this.render.bind(this));
+  this.physicsBodies = [];
+  this.vertSprings = [];
+  this.horzSprings = [];
+}
 
-  if(engine){
-    Engine.clear(engine);
-    reset();
+SurfReportAnimation.prototype.createBodies = function createBodies(days){
+  if(this.engine){
+    Engine.clear(this.engine);
+    this.reset();
   }
-
-  engine = Engine.create();
-
-
+  this.engine = Engine.create();
   for (var i = 0; i < days.length; i++) {
-    //create top SVG element
-    var hasTopElement = svgBods[i]
-    var rect = (hasTopElement)? svgBods[i] : document.createElementNS(svgns, 'rect');
-      var xCoord = width * i + (padding * (i+1));
-      rect.setAttributeNS(null, 'height', height);
-      rect.setAttributeNS(null, 'width', width);
-      var rfColor = (i === highlighted)? initHighlight : initColor;
-      rect.setAttributeNS(null, 'fill', rfColor);
-      rect.setAttributeNS(null, 'x', xCoord);
-      rect.setAttributeNS(null, 'class', "tip_svg");
-      if(!hasTopElement){
-        svgBods[i] = rect
-        svgBG.appendChild(rect);
-      }
-    //create base SVG element    
-    var hasBaseElement = svgBars[i]; 
-    var baseSVG = (hasBaseElement) ? svgBars[i] : document.createElementNS(svgns, 'rect');
-      baseSVG.setAttributeNS(null, 'height', height);
-      baseSVG.setAttributeNS(null, 'width', width);
-      var fColor = (i === highlighted)? initHighlight : initColor;
-      baseSVG.setAttributeNS(null, 'fill', fColor);
-      baseSVG.setAttributeNS(null, 'x', xCoord);
-      baseSVG.setAttributeNS(null, 'class', "base_svg");
-    if(!hasBaseElement){
-      svgBG.appendChild(baseSVG);
-      svgBars[i] = baseSVG;
-    }  
-
-    var xCoordP = 20 * i + (20 * (i+1));
-    var physicsBody = Bodies.rectangle(xCoordP, svgBoxDims.height, 2, 2);
-    var vertSpring = Constraint.create({ bodyA: physicsBody, pointB: { x: xCoordP, y: svgBoxDims.height } })
-    vertSpring.stiffness = 0.02;
-      // wave height
-    var spLen = (days[i].surfMax * 30 + (height))
-    vertSpring.length = spLen;
-    
-    var prevBody = physicsBodies[i-1]
-    if(prevBody){
-      var horizontalSpring = Constraint.create({ bodyA: physicsBody, bodyB: prevBody})
-      horizontalSpring.length = 20 * 2;//padding * 2; 
-      horizontalSpring.stiffness = 1;
-      horzSprings.push(horizontalSpring);
-    }
-    
-    rect.index = i;
-    rect.physicsBody = physicsBody;
-    
-    function mouseOverEvent(){
-        this.setAttributeNS(null, 'fill','#0c9fd5');
-        var day = days[this.index]
-        svgBars[this.index].setAttributeNS(null, 'fill', '#0c9fd5');
-        store.dispatch(hoverDay(this.index))
-      }
-
-    //rect.addEventListener('mouseover', mouseOverEvent.bind(rect));
-
-    baseSVG.addEventListener('mouseover', mouseOverEvent.bind(rect));
-    
-    function mouseOutEvent(){
-        var fColor = (this.index === highlighted)? initHighlight : initColor;
-        this.setAttributeNS(null, 'fill',fColor);
-         svgBars[this.index].setAttributeNS(null, 'fill', fColor);
-         svgBods[this.index].setAttributeNS(null, 'fill', fColor);
-        Body.setVelocity(physicsBodies[this.index], {x:0,y:-5}, {x:0.000,y:0.001});
-        this.setAttributeNS(null, 'height', height);
-        this.setAttributeNS(null, 'width', width);
-    }
-
-    baseSVG.addEventListener('click', function(){
-      var temp = highlighted;
-      highlighted = this.index
-      svgBods[temp].setAttributeNS(null, 'fill', initColor);
-      svgBars[temp].setAttributeNS(null, 'fill', initColor);
-    }.bind(rect))
-      
-      
-      //rect.addEventListener('mouseout', mouseOutEvent.bind(rect));
-      baseSVG.addEventListener('mouseout', mouseOutEvent.bind(rect));
-
-      physicsBodies.push(physicsBody);
-      vertSprings.push(vertSpring)
+    let topRect = this.createTopBar(i);
+    let bottomRect = this.createBottomBar(i);
+    let physicsBody = this.createPhysicsBodies(i, days);
+    topRect.index = i;
+    topRect.physicsBody = physicsBody;
+    topRect.parent = this; 
+    topRect.addEventListener('mouseover', mouseOverEvent.bind(topRect));
+    bottomRect.addEventListener('mouseover', mouseOverEvent.bind(topRect));
+    bottomRect.addEventListener('click', clickEvent.bind(topRect))
+    topRect.addEventListener('mouseout', mouseOutEvent.bind(topRect));
+    bottomRect.addEventListener('mouseout', mouseOutEvent.bind(topRect));
   }
 }
 
-function render() {
-  var bodies = Composite.allBodies(engine.world);
-  var constraints = Composite.allConstraints(engine.world);
-    for (var i = 0; i < bodies.length; i++) {
-        var vertices = bodies[i].vertices;
-        var rectSvg = svgBods[i];
-        if(rectSvg && svgBars && svgBars[i]){
-          rectSvg.setAttributeNS(null, 'y', vertices[0].y);
-          svgBars[i].setAttributeNS(null, 'height',svgBoxDims.height)
-          svgBars[i].setAttributeNS(null, 'y', vertices[0].y + height )
-        }
+SurfReportAnimation.prototype.createTopBar = function createTopBar(i){
+  let hasTopElement = this.svgBods[i]
+  // create top element if it doesn't already exist
+  let topRectSVG = (hasTopElement)? this.svgBods[i] : document.createElementNS(svgns, 'rect');
+  let xCoord = width * i + (padding * (i+1));
+  topRectSVG.setAttributeNS(null, 'height', height);
+  topRectSVG.setAttributeNS(null, 'width', width);
+  let rfColor = (i === this.highlighted)? initHighlight : initColor;
+  topRectSVG.setAttributeNS(null, 'fill', rfColor);
+  topRectSVG.setAttributeNS(null, 'x', xCoord);
+  topRectSVG.setAttributeNS(null, 'class', "tip_svg");
+  if(!hasTopElement){
+    this.svgBods[i] = topRectSVG
+    this.mainBackground.appendChild(topRectSVG);
+  }
+  return topRectSVG
+}
+
+SurfReportAnimation.prototype.createBottomBar = function createBottomBar(i){
+  let hasBaseElement = this.svgBars[i]; 
+  // create base element if it doesn't already exist
+    let baseRectSVG = (hasBaseElement) ? this.svgBars[i] : document.createElementNS(svgns, 'rect');
+    let xCoord = width * i + (padding * (i+1));
+    baseRectSVG.setAttributeNS(null, 'height', height);
+    baseRectSVG.setAttributeNS(null, 'width', width);
+    let fColor = (i === this.highlighted)? initHighlight : initColor;
+    baseRectSVG.setAttributeNS(null, 'fill', fColor);
+    baseRectSVG.setAttributeNS(null, 'x', xCoord);
+    baseRectSVG.setAttributeNS(null, 'class', "base_svg");
+    if(!hasBaseElement){
+      this.mainBackground.appendChild(baseRectSVG);
+      this.svgBars[i] = baseRectSVG;
+    }  
+    return baseRectSVG;
+}
+
+SurfReportAnimation.prototype.createPhysicsBodies = function createPhysicsBodies(i, days){
+  // create physics bodies
+  let xCoordP = 20 * i + (20 * (i+1));
+  let physicsBody = Bodies.rectangle(xCoordP, this.svgBoxDims.height, 2, 2);
+  let vertSpring = Constraint.create({ bodyA: physicsBody, pointB: { x: xCoordP, y: this.svgBoxDims.height } })
+  vertSpring.stiffness = 0.02;
+  let spLen = (days[i].surfMax * 30 + (height))
+  vertSpring.length = spLen;
+  
+  let prevBody = this.physicsBodies[i-1]
+  if(prevBody){
+    let horizontalSpring = Constraint.create({ bodyA: physicsBody, bodyB: prevBody})
+    horizontalSpring.length = 20 * 2; 
+    horizontalSpring.stiffness = 1;
+    this.horzSprings.push(horizontalSpring);
+  }
+  this.vertSprings.push(vertSpring);
+  this.physicsBodies.push(physicsBody);
+
+  return physicsBody;
+}
+
+
+SurfReportAnimation.prototype.render = function render() {
+  let bodies = Composite.allBodies(this.engine.world);
+  let constraints = Composite.allConstraints(this.engine.world);
+    for (let i = 0; i < bodies.length; i++) {
+      let vertices = bodies[i].vertices;
+      let rectSvg = this.svgBods[i];
+      if(rectSvg && this.svgBars && this.svgBars[i]){
+        rectSvg.setAttributeNS(null, 'y', vertices[0].y);
+        this.svgBars[i].setAttributeNS(null, 'height',this.svgBoxDims.height)
+        this.svgBars[i].setAttributeNS(null, 'y', vertices[0].y + height )
+      }
     }
 };
 
-var called = false;
+SurfReportAnimation.prototype.init = function init(data){
+  //create bodies will reset if already called
+  this.createBodies(data);
+  this.engine.world.gravity.y = -0.1;
+  World.add(this.engine.world, this.physicsBodies.concat(this.vertSprings.concat(this.horzSprings)));
+  // run the engines
+  this.mainAnimationLoop.addAnimation(this.matterEngine.bind(this));
+  this.mainAnimationLoop.addAnimation(this.render.bind(this));
+}
+
+SurfReportAnimation.prototype.matterEngine = function matterEngine(){
+  Engine.update(this.engine, 1000 / 60);
+}
+
+function clickEvent(){
+  let old = this.parent.highlighted;
+  this.parent.highlighted = this.index;
+  this.parent.svgBods[this.index].setAttributeNS(null, 'fill', initHighlight);
+  this.parent.svgBars[this.index].setAttributeNS(null, 'fill', initHighlight);
+  this.parent.svgBods[old].setAttributeNS(null, 'fill', initColor);
+  this.parent.svgBars[old].setAttributeNS(null, 'fill', initColor);
+}
+
+
+function mouseOverEvent(){
+  this.parent.svgBars[this.index].setAttributeNS(null, 'fill', '#0c9fd5');
+  this.parent.svgBods[this.index].setAttributeNS(null, 'fill', '#0c9fd5')
+  store.dispatch(hoverDay(this.index))
+}
+
+function mouseOutEvent(){
+  let fColor = (this.index === this.parent.highlighted)? initHighlight : initColor;
+  this.setAttributeNS(null, 'fill',fColor);
+  this.parent.svgBars[this.index].setAttributeNS(null, 'fill', fColor);
+  this.parent.svgBods[this.index].setAttributeNS(null, 'fill', fColor);
+  Body.setVelocity(this.physicsBody, {x:0,y:-5}, {x:0.000,y:0.001});
+  this.setAttributeNS(null, 'height', height);
+  this.setAttributeNS(null, 'width', width);
+}
+
+let animationInitialized;
+let el = document.getElementById('world');
+
 module.exports = function startSim(viz){ 
-  if(!called){
-    svgBG.style.opacity = 1;
-    svgBG.style.visibility = "visible";
-    called = true;
+  if(!animationInitialized){
+    animationInitialized = new SurfReportAnimation({
+      $el: el,
+      mainAnimationLoop: mainAnimationLoop
+    });
   }
-  startViz(viz);
+  let sim = (viz && surf[viz]) ? dataBuilder(surf[viz]) : ary;
+  mainAnimationLoop.setAnimationTimeout(animationInitialized.init.bind(animationInitialized, sim),1000);
 }
 
-function startViz(viz){
 
-  var sim = (viz && surf[viz]) ? dataBuilder(surf[viz]) : ary;
-  mainAnimationLoop.setAnimationTimeout(function(){
-      //create bodies will reset if already called
-      console.log(sim[0].regionAlias, sim[0])
-      createBodies(sim);
-      engine.world.gravity.y = -0.1;
-      World.add(engine.world, physicsBodies.concat(vertSprings.concat(horzSprings)));
-      // // run the engine
-      matterEngine = function matterEngine(){
-        Engine.update(engine, 1000 / 60)
-      }
-      mainAnimationLoop.addAnimation(matterEngine);
-      mainAnimationLoop.addAnimation(render);
-    },100);
-}
+
 
